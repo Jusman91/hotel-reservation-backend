5f5a921c2baa4aa70e55a9e2c4d396731d377fdb
Jusman91 <jusmannine1@gmail.com>
2023-12-02 16:19:58 +0800

    update users verify

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import { deleteUser, getAllUsers, getOneUser, updateUser } from '../controllers/user.js';
+import { verifyAdmin, verifyToken, verifyUser } from '../middleware/verifyToken.js';
 const router = express.Router();
 
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
-router.get('/:id', getOneUser)
-router.get('/', getAllUsers)
+// router.get('/checkauthentication', verifyToken, (req, res, next) => {
+//   res.send("hello user, you are now logged in")
+// })
+// router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+//   res.send("hello user, you are now logged in and you can delete your account")
+// })
+// router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
+//   res.send("hello admin, you are now logged in and you can delete all accounts")
+// })
+
+router.put('/:id', verifyUser, updateUser)
+router.delete('/:id', verifyUser, deleteUser)
+router.get('/:id', verifyUser, getOneUser)
+router.get('/', verifyAdmin, getAllUsers)
 
 export default router;
